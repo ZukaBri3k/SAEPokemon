01c7fd676e3b3f8b2c2e65564c290a48a0514e97
EwanP56 <ewanpenven@gmail.com>
2024-02-21 09:41:37 +0100

    oui

--- js/Type.js
+++ js/Type.js
@@ -1,1 +1,19 @@
-class
+class Type {
+    constructor(
+        form,
+        pokemon_id,
+        type
+    ) {
+        this.form = form;
+        this.pokemon_id = pokemon_id;
+        this.type = type;
+    }
+
+    toString() {
+        return `${this.form} ${this.type}`;
+    }
+}
+
+console.log(type[0]);
+
+let t1 = new Type("Normal", 1, "Grass");
