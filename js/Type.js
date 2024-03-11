89fd74deadfae8aff608d7c1a0b3fd1c92e7da6a
DUMERCHAT KYRILL <kdumerchat@mx21>
2024-03-11 15:27:29 +0100

    test

--- js/Type.js
+++ js/Type.js
@@ -13,9 +13,9 @@ class Type {
         return `${this.form} ${this.type}`;
     }
 }
-
+/* 
 console.log(pokemon_types[0]);
 
 let t1 = new Type("Normal", 1, "Grass");
 
-console.log(t1.toString());
+console.log(t1.toString()); */
